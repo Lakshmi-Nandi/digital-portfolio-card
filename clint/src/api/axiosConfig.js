db07873fb59784bb2488cd3e44459105e9c9633f
Lakshmi Nandi <lakshminandi554@gmail.com>
2025-09-27 09:37:43 +0530

    fix: update API URL to production

--- clint/src/api/axiosConfig.js
+++ clint/src/api/axiosConfig.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
-// --- This is the only line that needs to be changed ---
+// --- This is the corrected line ---
 // It now points to your live backend server on Render
 axios.defaults.baseURL = 'https://digital-portfolio-card-1.onrender.com/api';
-// --- End of change ---
+// --- End of correction ---
 
 // Function to set the authorization token
 const setAuthToken = (token) => {
   if (token) {
-    // Apply the token to every request header
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
-    // Delete the auth header
     delete axios.defaults.headers.common['Authorization'];
   }
 };
